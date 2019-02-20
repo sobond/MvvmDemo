0dec53b2c9ea7fcc6a8ec0861db7a0783e64ee2a
Daniel Soboń <sobondaniel@gmail.com>
2019-02-20 20:10:34 +0100

    Przygotowanie formularza z podzieałem na View, Model i ViewModel

--- MvvmDemo/MainWindow.xaml.cs
+++ MvvmDemo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvvmDemo.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,14 @@ namespace MvvmDemo
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StudentViewControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            StudentViewModel studentViewModelObject = new StudentViewModel();
+            studentViewModelObject.LoadStudents();
+
+            StudentViewControl.DataContext = studentViewModelObject;
         }
     }
 }
---
+++ MvvmDemo/ViewModel/StudentViewModel.cs
@@ -0,0 +1,30 @@
+﻿using MvvmDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmDemo.ViewModel
+{
+    class StudentViewModel
+    {
+        public ObservableCollection <Student> Students
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+
+            students.Add(new Student { FirstName = "Adam", LastName = "Nowak" });
+            students.Add(new Student { FirstName = "Jan", LastName = "Kowalski" });
+            students.Add(new Student { FirstName = "Michał", LastName = "Milowicz" });
+
+            Students = students;
+        }
+    }
+}
